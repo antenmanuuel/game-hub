85616a6b693fd3e820bc90156b9f2d7d61cfbbcf
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-05-27 11:06:01 -0400

    Build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
         lg: '"nav nav" "aside main"',
       }}
     >
-      <GridItem area="nav" bg="coral">
-        Nav
+      <GridItem area="nav">
+        <Navbar />
       </GridItem>
       <Show above="lg">
         <GridItem area="aside" bg="gold">
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import { HStack, Image, Text } from '@chakra-ui/react'
+import logo from '../assets/logo.webp'
+
+export const Navbar = () => {
+  return (
+    <HStack>
+        <Image src={logo} boxSize='60px' />
+        <Text>Navbar</Text>
+    </HStack>
+  )
+}
