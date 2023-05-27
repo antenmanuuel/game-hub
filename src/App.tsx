26feb1937e2ecab31b8f44a61273cba91e629548
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-05-27 11:19:03 -0400

    Build the color mode switch

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,11 @@ function App() {
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" bg="gold">
+        <GridItem area="aside">
           Aside
         </GridItem>
       </Show>
-      <GridItem area="main" bg="dodgerblue">
+      <GridItem area="main">
         Main
       </GridItem>
     </Grid>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from '@chakra-ui/react'
-import logo from '../assets/logo.webp'
+import { HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 export const Navbar = () => {
   return (
-    <HStack>
-        <Image src={logo} boxSize='60px' />
-        <Text>Navbar</Text>
+    <HStack justifyContent="space-between" padding="10px">
+      <Image src={logo} boxSize="60px" />
+      <ColorModeSwitch />
     </HStack>
-  )
-}
+  );
+};
