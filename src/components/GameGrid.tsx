100ea9baf4216e83a40111ebd7c21902c81299b0
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-05-29 12:26:21 -0400

    Clean up the game cards

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -8,7 +8,6 @@ import { GameQuery } from "../App";
 
 interface Props {
   gameQuery: GameQuery;
-  
 }
 
 const GameGrid = ({ gameQuery }: Props) => {
@@ -18,9 +17,9 @@ const GameGrid = ({ gameQuery }: Props) => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
-        spacing={3}
+        spacing={6}
       >
         {isLoading &&
           skeletons.map((skeleton) => (
