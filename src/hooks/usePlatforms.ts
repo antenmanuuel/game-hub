d0aa1623029218c0fd299db5d2d6367cc4e342fc
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-05-31 19:45:11 -0400

    Refactor -simply time calculations

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,8 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
-import platforms from '../data/platforms';
-import APIClient from '../services/api-client';
+import { useQuery } from "@tanstack/react-query";
+import platforms from "../data/platforms";
+import APIClient from "../services/api-client";
+import ms from "ms";
 
-const apiClient = new APIClient<Platform>('/platforms/lists/parents');
+const apiClient = new APIClient<Platform>("/platforms/lists/parents");
 
 export interface Platform {
   id: number;
@@ -12,10 +13,10 @@ export interface Platform {
 
 const usePlatforms = () =>
   useQuery({
-    queryKey: ['platforms'],
+    queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, //24h,
-    initialData: platforms
+    staleTime: ms("24h"),
+    initialData: platforms,
   });
 
 export default usePlatforms;
