c1c245480b5b159b55645f4afc8d41e46180e494
Anten Manuuel <anten.manuuel@stonybrook.edu>
2023-06-01 10:43:34 -0400

    Build game trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import useTrailers from "../hooks/UseTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+  if (isLoading) return null;
+  if (error) throw error;
+
+  const first = data?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.preview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
